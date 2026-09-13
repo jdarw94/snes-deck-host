c290117779141897d2308edb2745ecd66219f712
jdarw94 <jdarw2@gmail.com>
2026-09-14 06:14:57 +1000

    Update emulator code and start settings

--- main.js
+++ main.js
@@ -34,6 +34,7 @@ function launchEmulator(romBlobUrl) {
     window.EJS_core = "snes";
     window.EJS_gameUrl = romBlobUrl;
     window.EJS_pathtodata = "https://cdn.emulatorjs.org/stable/data/";
+    window.EJS_startOnLoaded = true;
 
     const script = document.createElement("script");
     script.src = "https://cdn.emulatorjs.org/stable/data/loader.js";
